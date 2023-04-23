18e1e88271d75c9398aa01a0a81e3249a4b476e1
Artem Zverev <anzverev1980@ya.ru>
2023-04-23 22:33:33 +0300

    tasks 35, 36
    tasks 35, 36

--- python_course/UNIT_4/UNIT_4/classwork/task35.py
+++ python_course/UNIT_4/UNIT_4/classwork/task35.py
@@ -1,4 +1,10 @@
 #todo: Заданы два списка. Необходимо их сериализовать в один файл.
 
+import pickle
+
 list_one = [True, 'If the implementation is hard to explain, it\'s a bad idea.', {'age': 27}]
 list_two = [False, 'Sparse is better than dense.', {'age': 90}]
+
+with open('file.pkl','ba+') as f:
+    pickle.dump(list_one,f)
+    pickle.dump(list_two,f)
--- python_course/UNIT_4/UNIT_4/classwork/task36.py
+++ python_course/UNIT_4/UNIT_4/classwork/task36.py
@@ -1,2 +1,21 @@
-#todo:
+# todo:
 # Реализовать декоратор который подсчитывает время выполнения функции.
+import time
+
+
+def time_of_function(function):
+    def wrapper(*args):
+        start = time.perf_counter_ns()
+        res = function(*args)
+        print(f'time of function', time.perf_counter_ns() - start)
+        return res
+
+    return wrapper
+
+
+@time_of_function
+def fun(x, y):
+    return x * y
+
+
+print(fun(10, 5))
